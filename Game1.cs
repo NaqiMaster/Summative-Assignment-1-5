a4bdfe0a5cc85f181d41fec84a12019ca74567ee
Naqi Master <701018804@GoTVDSB.ca>
2024-05-13 14:43:12 -0400

    Add Exit and Done

--- Game1.cs
+++ Game1.cs
@@ -22,6 +22,7 @@ namespace Summative_Assignment_1_5
         bowserJump,
         bowserStop,
         marioThrow,
+        Victory
     }
    /*  enum BowserPhase
     {
@@ -39,13 +40,12 @@ namespace Summative_Assignment_1_5
        // BowserPhase bowserPhase;
 
         Rectangle window, introExit, introContinue, mario, goldCoinRect,bowserRect,redShellRect,
-            endExit,endRestart;
-        Texture2D introScreen, textureExit, contentScreen, marioLeftTexture, marioRightTexture,
-            goldCoinTexture,marioCurrent, marioStraight,bowser,marioThrow,redShell,bowserKnockedTexture;
+            endExit,endRestart,textBubbleRect;
+        Texture2D introScreen, textureExit, contentScreen,marioTextureRight, marioTextureExcited,textBubble,
+            goldCoinTexture,marioTextureCurrent, marioTextureStraight,bowserTexture,marioTextureThrow,redShellTexture,marioTextureUp;
         MouseState mouseState, mouseState1;
-        SpriteFont introTitleFont,introDescription,exitText;
+        SpriteFont introTitleFont,introDescription,exitText,victorySpeech;
         Vector2 marioSpeed, bowserSpeed,redShellSpeed;
-        List <Texture2D> marioTextures = new List<Texture2D>();
         float seconds;
 
         private GraphicsDeviceManager _graphics;
@@ -69,10 +69,8 @@ namespace Summative_Assignment_1_5
             marioPhase = MarioPhase.InitialRun;
             seconds = 0f;
 
-            marioTextures.Add(marioLeftTexture);
-            marioTextures.Add(marioRightTexture);
 
-            
+            textBubbleRect = new Rectangle(270,180,200,50);
             goldCoinRect = new Rectangle(480, 250, 50, 50);
             introContinue = new Rectangle(50, 350, 210, 60);
             introExit = new Rectangle(50,420,210,60);
@@ -95,22 +93,27 @@ namespace Summative_Assignment_1_5
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-
+            //intro screen
             introScreen = Content.Load<Texture2D>("SuperMario");
             introTitleFont = Content.Load<SpriteFont>("introTitle");
             introDescription = Content.Load<SpriteFont>("introDescription");
             textureExit = Content.Load<Texture2D>("gold");
             exitText = Content.Load<SpriteFont>("exitText");
+
+            //Animation
+            victorySpeech = Content.Load<SpriteFont>("victorySpeech");
+
             contentScreen = Content.Load<Texture2D>("SuperMarioBackground");
-            marioLeftTexture = Content.Load<Texture2D>("marioLeft");
-            marioRightTexture = Content.Load<Texture2D>("marioRight");
+            marioTextureRight = Content.Load<Texture2D>("marioTextureRun");
             goldCoinTexture = Content.Load<Texture2D>("goldCoin");
-            marioStraight = Content.Load<Texture2D>("MarioStraight");
-            marioCurrent = Content.Load<Texture2D>("marioRight");
-            bowser = Content.Load<Texture2D>("Bowser");
-            marioThrow = Content.Load < Texture2D>("marioThrowing");
-            redShell = Content.Load<Texture2D>("redShell");
-            bowserKnockedTexture = Content.Load<Texture2D>("bowserKnocked");
+            marioTextureStraight = Content.Load<Texture2D>("mariotextureStand");
+            marioTextureUp = Content.Load<Texture2D>("marioTextureUp");
+            marioTextureCurrent = Content.Load<Texture2D>("marioTextureRun");
+            bowserTexture = Content.Load<Texture2D>("Bowser");
+            marioTextureThrow = Content.Load < Texture2D>("marioTextureThrow");
+            redShellTexture = Content.Load<Texture2D>("redShell");
+            marioTextureExcited = Content.Load<Texture2D>("marioTextureExcited");
+            textBubble = Content.Load<Texture2D>("textBubble");
 
             // TODO: use this.Content to load your game content here
         }
@@ -155,7 +158,7 @@ namespace Summative_Assignment_1_5
                 {
                     if (seconds >= 3)
                     {
-                        marioCurrent = marioStraight;
+                        marioTextureCurrent = marioTextureUp;
                         marioSpeed.X = 0;
                         marioSpeed.Y = -3;
                         marioPhase = MarioPhase.RunningRight1;
@@ -165,7 +168,7 @@ namespace Summative_Assignment_1_5
                 {
                     if (seconds >= 3.8 && !mario.Intersects(goldCoinRect))
                     {
-                        marioCurrent = marioRightTexture;
+                        marioTextureCurrent = marioTextureRight;
                         marioSpeed.X = 2;
                         marioSpeed.Y = 0;
                         marioPhase = MarioPhase.HitCoin;
@@ -178,7 +181,7 @@ namespace Summative_Assignment_1_5
                         marioSpeed.X = 0;
                         marioSpeed.Y = 0;
                         goldCoinRect = new Rectangle(0, 0, 0, 0);
-                        marioCurrent = marioStraight;
+                        marioTextureCurrent = marioTextureStraight;
                         bowserRect = new Rectangle(730, 250, 150, 150);
                         marioPhase = MarioPhase.bowserJump;
                         drawBowser = true;
@@ -206,7 +209,7 @@ namespace Summative_Assignment_1_5
                     bowserSpeed.X = 0;
                     bowserSpeed.Y = 0;
                     marioPhase = MarioPhase.marioThrow;
-                    marioCurrent = marioThrow;
+                    marioTextureCurrent = marioTextureThrow;
                     redShellRect = new Rectangle(450, 270, 30, 30);
                     redShellSpeed.X = 10;
                     redShellSpeed.Y = 0;
@@ -221,10 +224,17 @@ namespace Summative_Assignment_1_5
                     if (bowserRect.Contains(redShellRect.X, redShellRect.Y))
                     {
                         redShellRect = new Rectangle(0, 0, 0, 0);
-                        marioCurrent = marioStraight;
-
                     }
 
+                    if (bowserRect.X >= 950)
+                    {
+                        marioPhase = MarioPhase.Victory;
+                    }
+
+                }
+                else if (marioPhase == MarioPhase.Victory)
+                {
+                    marioTextureCurrent = marioTextureExcited;
                 }
             }
             else if (screen == Screen.End)
@@ -272,18 +282,24 @@ namespace Summative_Assignment_1_5
             else if (screen == Screen.MainAnimation)
             {
                 _spriteBatch.Draw(contentScreen, window, Color.White);
-                _spriteBatch.Draw(marioCurrent, mario, Color.White);
+                _spriteBatch.Draw(marioTextureCurrent, mario, Color.White);
                 _spriteBatch.Draw(goldCoinTexture, goldCoinRect,Color.White);
 
                if (drawBowser)
-                {
-                    _spriteBatch.Draw(bowser,bowserRect,Color.White);
+               {
+                    _spriteBatch.Draw(bowserTexture,bowserRect,Color.White);
 
-                }
-                if (marioPhase == MarioPhase.marioThrow)
-                {
-                    _spriteBatch.Draw(redShell, redShellRect, Color.White);
+               }
+               if (marioPhase == MarioPhase.marioThrow)
+               {
+                    _spriteBatch.Draw(redShellTexture, redShellRect, Color.White);
 
+               }
+
+               if (marioPhase == MarioPhase.Victory)
+                {
+                    _spriteBatch.Draw(textBubble, textBubbleRect, Color.White);
+                    _spriteBatch.DrawString(victorySpeech, ("WE GOT HIM!"), new Vector2(280, 190), Color.Black);
                 }
             }
             else if (screen == Screen.End)
